046ef4bb380e89ed41e7244c472b409fe8919218
Crowdbotics <team@crowdbotics.com>
2024-01-24 02:48:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "myk-ecom-checkout-45498",
+  url: "https://myk-ecom-checkout-45498.botics.co",
+  api: "https://myk-ecom-checkout-45498.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
